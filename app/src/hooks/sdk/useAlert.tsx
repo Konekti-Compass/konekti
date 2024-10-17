7e59d259bcc2edf8a3a77a7ec0a5a47cae190d46
Yushin Ito <75526539+yushin-ito@users.noreply.github.com>
2024-10-17 14:36:44 +0900

    リファクタリング

--- app/src/hooks/sdk/useAlert.tsx
+++ app/src/hooks/sdk/useAlert.tsx
@@ -1,42 +1,28 @@
 import { useToast } from "native-base";
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback } from "react";
 
 import Alert from "../../components/molecules/Alert";
 
 const useAlert = () => {
   const toast = useToast();
 
-  const [isActive, setIsActive] = React.useState(false);
-  const [id, setId] = React.useState(Math.random());
-
-  useEffect(() => {
-    (async () => {
-      setIsActive(toast.isActive(1));
-    })();
-  }, [toast]);
-
   const showAlert = useCallback(
     async ({ text, status }: { text: string; status: "success" | "error" }) => {
       toast.closeAll();
-      if (!isActive) {
-        const newId = Math.random();
-        toast.show({
-          id: newId,
-          duration: 3000,
-          placement: "top",
-          render: () => (
-            <Alert
-              text={text}
-              status={status}
-              onPressCloseButton={() => toast.close(1)}
-            />
-          ),
-        });
-        setId(newId);
-        setIsActive(true);
-      }
+      toast.show({
+        id: Math.random(),
+        duration: 3000,
+        placement: "top",
+        render: () => (
+          <Alert
+            text={text}
+            status={status}
+            onPressCloseButton={() => toast.close(1)}
+          />
+        ),
+      });
     },
-    [toast, isActive]
+    []
   );
 
   return {

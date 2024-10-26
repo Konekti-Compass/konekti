f4f31ec2bd22edccf19c9b5771cb1884347878fb
yushin_ito <yitengyouxin8@gmail.com>
2024-10-26 23:19:55 +0900

    fix: Androidのバグ

--- app/src/components/templates/QRCodeTemplate.tsx
+++ app/src/components/templates/QRCodeTemplate.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React from "react";
 import { useWindowDimensions } from "react-native";
 
 import { Feather } from "@expo/vector-icons";
@@ -18,127 +18,137 @@ import Overlay from "../molecules/Overlay";
 import QRCodeActionSheet from "../organisms/QRCodeActionSheet";
 
 type QRCodeTemplateProps = {
+  profileId: number;
   isLoadingSearchProfileByProfileId: boolean;
   searchProfileByProfileId: (profileId: number) => Promise<void>;
   goBackNavigationHandler: () => void;
 };
 
-const QRCodeTemplate = memo(
-  ({
-    isLoadingSearchProfileByProfileId,
-    searchProfileByProfileId,
-    goBackNavigationHandler,
-  }: QRCodeTemplateProps) => {
-    const { width } = useWindowDimensions();
+const QRCodeTemplate = ({
+  profileId,
+  isLoadingSearchProfileByProfileId,
+  searchProfileByProfileId,
+  goBackNavigationHandler,
+}: QRCodeTemplateProps) => {
+  const { width } = useWindowDimensions();
 
-    const { isOpen, onOpen, onClose } = useDisclose();
+  const { isOpen, onOpen, onClose } = useDisclose();
 
-    return (
-      <Box flex={1}>
-        <Overlay isOpen={isLoadingSearchProfileByProfileId} />
-        <QRCodeActionSheet value="test" isOpen={isOpen} onClose={onClose} />
-        <Box w={`${width}px`} h="540px">
-          <CameraView
-            barcodeScannerSettings={{ barcodeTypes: ["qr"] }}
-            onBarcodeScanned={({ data }) =>
-              searchProfileByProfileId(Number(data))
-            }
-            style={{ width: "100%", height: "100%" }}
-          >
-            <Box position="relative" flex={1}>
-              <Box
-                position="absolute"
-                top="0px"
-                left="0px"
-                bottom="0px"
-                right={`${width - (width - 320) / 2 - 5}px`}
-                bg="black"
-                opacity="0.4"
+  return (
+    <Box flex={1}>
+      <Overlay isOpen={isLoadingSearchProfileByProfileId} />
+      <QRCodeActionSheet
+        value={profileId.toString()}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
+      <Box w={`${width}px`} h="540px">
+        <CameraView
+          barcodeScannerSettings={{ barcodeTypes: ["qr"] }}
+          onBarcodeScanned={({ data }) =>
+            searchProfileByProfileId(Number(data))
+          }
+          style={{ width: "100%", height: "100%" }}
+        >
+          <Box w="100%" h="100%" position="relative">
+            <Box
+              position="absolute"
+              top="0px"
+              left="0px"
+              bottom="0px"
+              right={`${width - (width - 320) / 2 - 5}px`}
+              bg="black"
+              opacity="0.4"
+              zIndex={0}
+            />
+            <Box
+              position="absolute"
+              top="0px"
+              left={`${width - (width - 320) / 2 - 5}px`}
+              bottom="0px"
+              right="0px"
+              bg="black"
+              opacity="0.4"
+              zIndex={0}
+            />
+            <Box
+              position="absolute"
+              top="0px"
+              left={`${(width - 320) / 2 + 5}px`}
+              bottom={`${540 - ((540 - 320) / 2 + 25) - 5}px`}
+              right={`${(width - 320) / 2 + 5}px`}
+              bg="black"
+              opacity="0.4"
+              zIndex={0}
+            />
+            <Box
+              position="absolute"
+              top={`${540 - ((540 - 320) / 2 - 25) - 5}px`}
+              left={`${(width - 320) / 2 + 5}px`}
+              bottom="0px"
+              right={`${(width - 320) / 2 + 5}px`}
+              bg="black"
+              opacity="0.4"
+              zIndex={0}
+            />
+            <Box
+              position="absolute"
+              top={`${(540 - 320) / 2 + 25}px`}
+              left={`${(width - 320) / 2}px`}
+              w="320px"
+              h="320px"
+              borderColor="white"
+              borderWidth="5px"
+              rounded="20"
+              zIndex={1}
+            />
+            <HStack
+              px="2"
+              w="100%"
+              alignItems="center"
+              justifyContent="space-between"
+              position="absolute"
+              zIndex={1}
+              safeAreaTop
+            >
+              <IconButton
+                onPress={goBackNavigationHandler}
+                icon={
+                  <Icon
+                    as={<Feather name="chevron-left" />}
+                    size="2xl"
+                    color="white"
+                  />
+                }
+                variant="unstyled"
               />
-              <Box
-                position="absolute"
-                top="0px"
-                left={`${width - (width - 320) / 2 - 5}px`}
-                bottom="0px"
-                right="0px"
-                bg="black"
-                opacity="0.4"
+              <IconButton
+                onPress={goBackNavigationHandler}
+                icon={
+                  <Icon as={<Feather name="x" />} size="xl" color="white" />
+                }
+                variant="unstyled"
               />
-              <Box
-                position="absolute"
-                top="0px"
-                left={`${(width - 320) / 2 + 5}px`}
-                bottom={`${540 - ((540 - 320) / 2 + 25) - 5}px`}
-                right={`${(width - 320) / 2 + 5}px`}
-                bg="black"
-                opacity="0.4"
-              />
-              <Box
-                position="absolute"
-                top={`${540 - ((540 - 320) / 2 - 25) - 5}px`}
-                left={`${(width - 320) / 2 + 5}px`}
-                bottom="0px"
-                right={`${(width - 320) / 2 + 5}px`}
-                bg="black"
-                opacity="0.4"
-              />
-              <Box
-                position="absolute"
-                top={`${(540 - 320) / 2 + 25}px`}
-                left={`${(width - 320) / 2}px`}
-                w="320px"
-                h="320px"
-                borderColor="white"
-                borderWidth="5px"
-                rounded="20"
-              />
-              <HStack
-                px="2"
-                w="100%"
-                alignItems="center"
-                justifyContent="space-between"
-                position="absolute"
-                safeAreaTop
-              >
-                <IconButton
-                  onPress={goBackNavigationHandler}
-                  icon={
-                    <Icon
-                      as={<Feather name="chevron-left" />}
-                      size="2xl"
-                      color="white"
-                    />
-                  }
-                  variant="unstyled"
-                />
-                <IconButton
-                  onPress={goBackNavigationHandler}
-                  icon={
-                    <Icon as={<Feather name="x" />} size="xl" color="white" />
-                  }
-                  variant="unstyled"
-                />
-              </HStack>
-            </Box>
-          </CameraView>
-        </Box>
-        <VStack alignItems="center">
-          <Link
-            mt="6"
-            _text={{ color: "brand.600", fontSize: "lg" }}
-            onPress={() => onOpen()}
-          >
-            QRコードを表示する
-          </Link>
-          <HStack>
-            <Box>
-              <Text></Text>
-            </Box>
-          </HStack>
-        </VStack>
+            </HStack>
+          </Box>
+        </CameraView>
       </Box>
-    );
-  },
-);
+      <VStack alignItems="center">
+        <Link
+          mt="6"
+          _text={{ color: "brand.600", fontSize: "lg" }}
+          onPress={() => onOpen()}
+        >
+          QRコードを表示する
+        </Link>
+        <HStack>
+          <Box>
+            <Text></Text>
+          </Box>
+        </HStack>
+      </VStack>
+    </Box>
+  );
+};
 
 export default QRCodeTemplate;
